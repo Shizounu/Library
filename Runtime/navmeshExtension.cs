a03e9a8cc52a862e6d8d1452de71e3c69b2b4ae1
Shizounu <43500949+Shizounu@users.noreply.github.com>
2021-06-15 12:54:10 +0200

    added navmesh extension

---
+++ Runtime/navmeshExtension.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Shizounu.Library{
+    public static class NavMeshPathExtension{
+		public static float PathLength(this NavMeshPath path){
+			float length = 0;
+			for (int i = 1; i < path.corners.Length; i++){
+				length += Vector3.Distance(path.corners[i - 1], path.corners[i]);
+			}
+			return length;
+		}   
+    }
+}
