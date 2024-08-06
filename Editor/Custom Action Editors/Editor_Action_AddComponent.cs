7ede65a3ff89fb4c03a5af8b480765b91251af93
Shizounu <erik@simon-potsdam.de>
2024-08-07 00:50:02 +0200

    Small Additions

--- Editor/Custom Action Editors/Editor_Action_AddComponent.cs
+++ Editor/Custom Action Editors/Editor_Action_AddComponent.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-
-[CustomEditor(typeof(Action_AddComponent))]
-public class Editor_Action_AddComponent : Editor {
-    private void drawDropdown(Action_AddComponent so){
-        TypeCache.TypeCollection collection = TypeCache.GetTypesDerivedFrom<Component>();
+namespace Shizounu.Library.Editor.StateMachineEditor
+{
+    [CustomEditor(typeof(Action_AddComponent))]
+    public class Editor_Action_AddComponent : UnityEditor.Editor {
+        private void DrawDropdown(Action_AddComponent so){
+            TypeCache.TypeCollection collection = TypeCache.GetTypesDerivedFrom<Component>();
         
-        List<string> names = new();
-        foreach (System.Type type in collection)
-            names.Add(type.ToString());
+            List<string> names = new();
+            foreach (System.Type type in collection)
+                names.Add(type.ToString());
 
-        int index = (so.typeToAdd != null ? collection.IndexOf(so.typeToAdd) : 0); 
-        int newIndex = EditorGUILayout.Popup(index,names.ToArray());
-        so.typeToAdd = collection[newIndex];
-    }
-    public override void OnInspectorGUI() {
-        Action_AddComponent SO = target as Action_AddComponent; 
-        drawDropdown(SO);
+            int index = (so.typeToAdd != null ? collection.IndexOf(so.typeToAdd) : 0); 
+            int newIndex = EditorGUILayout.Popup(index,names.ToArray());
+            so.typeToAdd = collection[newIndex];
+        }
+        public override void OnInspectorGUI() {
+            Action_AddComponent SO = target as Action_AddComponent; 
+            DrawDropdown(SO);
         
-        base.OnInspectorGUI();
+            base.OnInspectorGUI();
+        }
     }
+
 }
